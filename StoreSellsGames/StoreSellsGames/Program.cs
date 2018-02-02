95842de39c8d3a3e0462eab69d5edc76b18742cb
Piitaq91 <piitaq91@github.com>
2018-02-02 11:49:11 +0100

    first draft

---
+++ StoreSellsGames/StoreSellsGames/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+public class Game
+{
+    string name;
+    string genre;
+    int esrRating;
+    float price;
+    float rating;
+
+
+    public Game()
+    {
+        
+    }
+
+    public void BuyGame(Game gameToBuy)
+    {
+        //what do we need after selling the game, f.eks. delete the game from store
+    }
+}
+
+
+public class Store
+{
+    string gamesAvailable;
+    float price;
+
+    public void BuyGame(Game gameToBuy)
+    {
+        
+    }
+}
+
+
+public class DLC
+{
+    public Game game;
+    float DLCprice;
+    float rating;
+
+    public void BuyDLC(DLC dlcToBuy)
+    {
+        
+    }
+}
+
+
+public class Showcase
+{
+    
+}
+
+
+public class Users
+{
+    //man kan lave en previous bought games, to keep track off the money we are making
+}
